72f70458658d5fa97b4f4ac559a2d083e81c5f0e
Richard L Huynh <huynhtastic@gmail.com>
2017-12-04 01:28:30 -0600

    Contracts now importing correctly
    added .env for absolute imports
    symlinked build/contracts/ into src
    migrated css to antd

--- src/components/votingtable/VotingTable.js
+++ src/components/votingtable/VotingTable.js
@@ -1,18 +1,34 @@
-import * as network from '../../services/truffle.js';
-import Web3 from 'web3';
 import React, { Component } from 'react';
 import { Table } from 'antd';
 
 class VotingTable extends Component {
   render() {
-    const devNet = network.networks.development;
-    const devUrl = `http://${devNet.host}:${devNet.port}`;
-    var web3 = new Web3(devUrl);
-    //    var VotingContract = new web3.eth.Contract(votingContractCode.abi);
+    const columns = [{
+      title: 'Candidate',
+      dataIndex: 'candidate',
+      key: 'candidate',
+    }, {
+      title: 'Votes',
+      dataIndex: 'votes',
+      key: 'votes',
+    }];
+    const dataSource = [{
+      key: '1',
+      candidate: 'Richard',
+      votes: 0
+    }, {
+      key: '2',
+      candidate: 'Ronly',
+      votes: 0
+    }, {
+      key: '3',
+      candidate: 'Mary',
+      votes: 0
+    }];
 
 
     return (
-      <h1>Placeholder</h1>
+      <Table columns={columns} dataSource={dataSource} />
     )
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,38 @@
+import { Layout, Row, Col } from 'antd';
+import 'antd/dist/antd.css';
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import { Row, Col } from 'antd';
-import VotingTable from './components/votingtable/VotingTable.js';
+import Web3 from 'web3';
+
+import * as network from 'services/truffle.js';
+import VotingTable from 'components/votingtable/VotingTable.js';
+import * as VotingContract from 'contracts/Voting.json';
+
+const { Header, Content, Footer } = Layout;
 
 class App extends Component {
   render() {
+    const devNet = network.networks.development;
+    const devUrl = `http://${devNet.host}:${devNet.port}`;
+    var web3 = new Web3(devUrl);
+    var accounts;
+
+    web3.eth.getAccounts().then((accs) => {
+      accounts = accs;
+    });
+
+
     return (
-      <Row>
-        <Col span={12}>
-          <h1>A Simple Hello World Application</h1>
-          <VotingTable />
-        </Col>
-      </Row>
+      <Layout className='layout'>
+        <Content style={{ padding: '0 50px', textAlign: 'center' }}>
+          <Row type='flex' justify='center'>
+            <Col span={12}>
+              <h1>A Simple Hello World Application</h1>
+              <VotingTable />
+            </Col>
+          </Row>
+        </Content>
+      </Layout>
     )
   }
 }
